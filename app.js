12581537568252161bdfabf6c42db88151f7e482
Radha Sharma <radhasharma2786@gmail.com>
2023-08-13 19:15:42 +0530

    version-1(added crud functionalities)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 5000;
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const methodOverride=require('method-override'); // for sending patch request
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/shopping-app")
   .then(() => {
@@ -19,6 +21,10 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("ejs", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
+
 app.listen(port, () => {
   console.log(`app listening at port ${port}`);
 });
--- routes/products.js
+++ routes/products.js
@@ -8,8 +8,44 @@ router.get("/products", async (req, res) => {
   res.render("products/index", { products });
 });
 
-router.get('/products/new',async(req,res)=>{
-  res.render('products/new');
+router.get("/products/new", async (req, res) => {
+  res.render("products/new");
+});
+
+router.post("/products", async (req, res) => {
+  const { name, img, price, desc } = req.body;
+  await Product.create({ name,img, price, desc });
+  res.redirect("/products");
+});
+
+// display a particular product 
+
+router.get('/products/:id',async(req,res)=>{
+  const {id} = req.params;
+  const product=await Product.findById(id);
+  res.render('products/show',{product});
+})
+
+// edit a particular product
+
+router.get('/products/:id/edit', async(req,res)=>{
+  const {id} = req.params;
+  const product=await Product.findById(id);
+  res.render('products/edit',{product});
+})
+
+router.patch('/products/:id',async(req,res)=>{
+  const {id} =  req.params;
+  const{name,img,price,desc}=req.body;
+  await Product.findByIdAndUpdate(id,{name,img,price,desc});
+  res.redirect(`/products/${id}`);
+})
+
+router.delete('/products/:id',async(req,res)=>{
+  const {id} = req.params ;
+  await Product.findByIdAndDelete(id);
+  res.redirect('/products');
 })
 
 module.exports = router;
+
